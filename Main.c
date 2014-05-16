144de71e6041ab9ba8b0f2b840517b4590f59245
MarutMark <markmarut@gmail.com>
2014-05-16 10:06:40 -0400

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,1 +1,17 @@
-System.out.print("Hey guys just some funtimes in 4th period Cs") 
+import java.awt.*;
+import java.awt.event.*;
+import java.swing.*;
+public class Frame1 extends JFrame
+{
+  JPanel pane = new JPanel();
+  Frame1() 
+  {
+    super("My Simple Frame"); setBounds(100,100,300,100);
+    setDefaultCloseOperation(JFrame.Exit_ON_ClOSE);
+    Container con = this.getContentPane();
+    con.add(pane);
+    pane.add(FirstPane);
+    setVisible(true);
+  }
+  public static void main (String args[]){new Frame1()};}
+  }
